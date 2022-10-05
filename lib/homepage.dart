88dd1df82d2cfe0ed81cd93ed45d1c574fc2ba76
afiiq <wafiqmuhaz@gmail.com>
2022-10-05 22:54:35 +0700

    Initial commit

---
+++ lib/homepage.dart
@@ -0,0 +1,25 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'package:flutter/material.dart';
+import 'package:responsivetutorial/responsive/desktop_body.dart';
+import 'package:responsivetutorial/responsive/mobile_body.dart';
+import 'package:responsivetutorial/responsive/responsive_layout.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ResponsiveLayout(
+        mobileBody: MyMobileBody(),
+        desktopBody: MyDesktopBody(),
+      ),
+    );
+  }
+}
